b8a511e787a4db8346372d1a9a24e9c5bacfa483
Maddy Haldeman and Tanner Damron <maddy@>
2019-01-14 11:37:07 -0800

    create project file and js script that will make several unit conversions.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//gallons to liters
+var gallon = parseInt(prompt("Enter the amount in gallons"));
+
+var result = gallon * 3.785412;
+
+//ounces to grams
+var ounces = parseInt(prompt("Enter the amount in ounces"));
+
+var result = ounces / 28.35;
+
+//teaspoons to tablespoons
+var tablespoons = parseInt(prompt("Enter the number of tablespoons"));
+
+var result = tablespoons / 3;
+
+alert(result)
